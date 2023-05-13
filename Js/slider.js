7afe82d87ec23f8ca7c6653f1366ce2f8621433a
Pomiraiu <101802475+Pomiraiu@users.noreply.github.com>
2023-05-13 22:28:11 +0300

    Add files via upload

---
+++ Js/slider.js
@@ -0,0 +1,14 @@
+const slider = $(".products").bxSlider({
+    pager: false,
+    controls: false,
+  });
+  
+  $(".product-slider__arrow--direction--left").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+  });
+  
+  $(".product-slider__arrow--direction--right").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+  });
